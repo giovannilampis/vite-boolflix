1805c18f317e8d7cf7d8290707c751be859a8ed9
giovannilampis <giolampis@hotmail.com>
2023-05-09 11:58:45 +0200

    add parameters > api_kei to store.js

--- store.js
+++ store.js
@@ -2,5 +2,8 @@ import { reactive } from "vue";
 
 export const store = reactive({
     baseUrl: 'https://api.themoviedb.org/3/',
-    endPointMovie: '/search/movie'
+    endPointMovie: '/search/movie',
+    parameters: {
+        api_key: '9af83964190f4e1b60da985a0d3087d0'
+    }
 })
