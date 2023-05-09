1efe8b894eecee64000c8164d85a2dae1d70a12c
giovannilampis <giolampis@hotmail.com>
2023-05-09 15:23:20 +0200

    add ItemSingle.vue component

--- src/assets/components/ItemList.vue
+++ src/assets/components/ItemList.vue
@@ -1,20 +1,34 @@
 <script>
+import ItemSingleVue from './ItemSingle.vue'
 
 export default {
     name: 'ItemList',
     components: {
-
+        ItemSingle
     }
 }
 
 </script>
 
 <template>
-    <section>
+
+    <section class="container">
 
         <h2 class="text-uppercase">movies</h2>
+
+        <div class="row">
+
+            <div class="col-12 col-md-4 col-lg-2">
+
+                <ItemSingle/>
+
+            </div>
+
+
+        </div>
         
     </section>
+
 </template>
 
 <style scoped>
---
+++ src/assets/components/ItemSingle.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    name: 'ItemSingle',
+    components: {
+
+    }
+}
+
+</script>
+
+<template>
+    
+</template>
+
+<style scoped>
+
+
+</style>
