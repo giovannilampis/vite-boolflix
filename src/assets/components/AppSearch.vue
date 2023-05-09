37736882f64fa0ceac7e53bf7418a11c919b0105
giovannilampis <giolampis@hotmail.com>
2023-05-09 12:55:27 +0200

    add AppSearch.vue component

---
+++ src/assets/components/AppSearch.vue
@@ -0,0 +1,33 @@
+<script>
+
+name: 'AppSearch',
+data() {
+    return {
+        text: ''
+    }
+},
+
+
+</script>
+
+<template>
+    <form class="d-flex justify-content-center align-items-center">
+
+        <div class="me-2">
+            <label class="visually-hidden" for="titlename">earch a title</label>
+
+            <input type="text" id="titlename" class="form-control" v-model.trim="text" placeholder="Search a title">
+        </div>
+
+        <div>
+            <button type="submit" class="btn btn-primary"></button>
+        </div>
+       
+        
+    </form>
+</template>
+
+
+<style scoped>
+
+</style>
